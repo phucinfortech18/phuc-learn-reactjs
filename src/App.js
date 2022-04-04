8340eb849b43e44d6f771631aef39278afa65313
Anh Phuc <phucinfortech18@gmail.com>
2022-04-04 23:00:04 +0700

    update text

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Anh Phuc Learn ReactJS</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Anh Phuc Learn ReactJS!!!!</p>
       </header>
     </div>
   );
