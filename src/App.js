40a35e9c294bd06124ee0ddf3ab7e95a68a8bb8f
Anh Phuc <phucinfortech18@gmail.com>
2022-10-11 11:06:28 +0700

    render data co ban

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "Phuc";
+  const age = 18;
+  const isFemale = false;
+  const student = {
+    name: "Easy Frontend",
+  };
+  const colorList = ["red", "green", "blue"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Anh Phuc Learn ReactJS!!!! new new</p>
-        <div>Learn Git </div>
-        <div>Learn Git 2 </div>
-        <div>Learn Git 3</div>
-        <div>Learn Git 4</div>
-        <h1>anh Phúc học reactJS - 2022</h1>
-        <h1>anh Phúc học reactJS - 2023</h1>
-        <h1>anh Phúc học reactJS - 2024</h1>
-        <h1>anh Phúc học reactJS - 2025</h1>
-        <h1>anh Phúc học reactJS - 2026</h1>
+        <p>
+          Xin chao {name} - {age}
+        </p>
+        <p>
+          <i>Gioi tinh:</i> {isFemale ? <span>Female</span> : <span>Male</span>}
+        </p>
+
+        <ul>
+          {colorList.map((color) => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
+
+        <p>{student.name}</p>
       </header>
     </div>
   );
